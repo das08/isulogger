116ed8c815006ba8bef63522a4500ac35610cf15
das08 <41512077+das08@users.noreply.github.com>
2022-08-09 22:54:52 +0900

    CREATE TABLE

--- db/init/00_init.sql
+++ db/init/00_init.sql
@@ -1,3 +1,15 @@
 CREATE DATABASE isulogger;
 
 \c isulogger
+
+CREATE TABLE entry (
+    id serial NOT NULL,
+    isucon_id integer NOT NULL,
+    timestamp timestamp without time zone NOT NULL,
+    score int not null default 0,
+    message text NOT NULL default '',
+    access_log_path text default NULL,
+    slow_log_path text default NULL,
+    image_path text default NULL,
+    PRIMARY KEY (id)
+);
