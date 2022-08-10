de2e7a8fd4bb52c16afedde49e441d78e831715e
das08 <41512077+das08@users.noreply.github.com>
2022-08-10 11:27:30 +0900

    use axios

--- app/src/components/LogEntry.vue
+++ app/src/components/LogEntry.vue
@@ -18,33 +18,34 @@
 <script>
 import { defineComponent, reactive } from "vue";
 import TableLite from "vue3-table-lite";
+import axios from "axios";
 
 // Fake Data for 'asc' sortable
-const sampleData1 = (offst, limit) => {
-  offst = offst + 1;
-  let data = [];
-  for (let i = offst; i <= limit; i++) {
-    data.push({
-      timestamp: new Date( 2022, 8, 2, i%60, 15, 30 ),
-      score: i*1000+60,
-      message: "test" + i,
-    });
-  }
-  return data;
-};
+// const sampleData1 = (offst, limit) => {
+//   offst = offst + 1;
+//   let data = [];
+//   for (let i = offst; i <= limit; i++) {
+//     data.push({
+//       timestamp: new Date( 2022, 8, 2, i%60, 15, 30 ),
+//       score: i*1000+60,
+//       message: "test" + i,
+//     });
+//   }
+//   return data;
+// };
 
 // Fake Data for 'desc' sortable
-const sampleData2 = (offst, limit) => {
-  let data = [];
-  for (let i = limit; i > offst; i--) {
-    data.push({
-      timestamp: new Date( 2022, 8, 2, i%60, 15, 30 ),
-      score: i*1000+60,
-      message: "test" + i,
-    });
-  }
-  return data;
-};
+// const sampleData2 = (offst, limit) => {
+//   let data = [];
+//   for (let i = limit; i > offst; i--) {
+//     data.push({
+//       timestamp: new Date( 2022, 8, 2, i%60, 15, 30 ),
+//       score: i*1000+60,
+//       message: "test" + i,
+//     });
+//   }
+//   return data;
+// };
 
 export default defineComponent({
   name: "App",
@@ -135,20 +136,17 @@ export default defineComponent({
      */
     const doSearch = (offset, limit, order, sort) => {
       table.isLoading = true;
-      setTimeout(() => {
-        table.isReSearch = offset == undefined ? true : false;
-        if (offset >= 10 || limit >= 20) {
-          limit = 20;
-        }
-        if (sort === "asc") {
-          table.rows = sampleData1(offset, limit);
-        } else {
-          table.rows = sampleData2(offset, limit);
-        }
-        table.totalRecordCount = 20;
-        table.sortable.order = order;
-        table.sortable.sort = sort;
-      }, 600);
+      let url = "http://localhost:8082/get?isucon_id=1";
+      axios.get(url)
+          .then((response) => {
+            console.log(response.data);
+
+            // refresh table rows
+            table.rows = response.data;
+            table.totalRecordCount = response.data.length;
+            table.sortable.order = order;
+            table.sortable.sort = sort;
+          });
     };
 
     /**
@@ -191,12 +189,14 @@ export default defineComponent({
 
 // convert timestamp to date
 function convertTimestamp(timestamp) {
-  var year = timestamp.getFullYear();
-  var month = timestamp.getMonth();
-  var day = timestamp.getDate();
-  var hour = timestamp.getHours();
-  var min = timestamp.getMinutes();
-  var sec = timestamp.getSeconds();
+  let parsed = Date.parse(timestamp);
+  let date = new Date(parsed);
+  var year = date.getFullYear();
+  var month = date.getMonth();
+  var day = date.getDate();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
   return year + "/" + month + "/" + day + " " + hour + ":" + min + ":" + sec;
 }
 </script>
