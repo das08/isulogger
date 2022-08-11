2e3031ea46d006e1bf6784744024128e88bd93b5
das08 <41512077+das08@users.noreply.github.com>
2022-08-11 17:26:13 +0900

    add hamburger menu

--- app/src/App.vue
+++ app/src/App.vue
@@ -30,7 +30,28 @@
       </div>
 
       <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
     </v-app-bar>
+    <v-navigation-drawer
+        v-model="drawer"
+        fixed
+        right
+        temporary
+    >
+      <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.link"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view></router-view>
@@ -46,7 +67,12 @@ export default {
   },
 
   data: () => ({
-    //
+    drawer: false,
+    items: [
+      {title: 'ホーム', icon: 'mdi-home', link: {name: 'HelloWorld'}},
+      {title: 'ログを見る', icon: 'mdi-text-box-multiple', link: {name: 'Logs'}},
+      {title: 'コンテスト作成', icon: 'mdi-plus-circle', link: {name: 'Create Contest'}},
+    ]
   }),
 };
 </script>
