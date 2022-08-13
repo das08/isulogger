639a666955d4fb261054969f610d5ab886bf4955
das08 <41512077+das08@users.noreply.github.com>
2022-08-13 21:54:30 +0900

    top page

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -12,38 +12,25 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Welcome to ISULOGGER
         </h1>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
+        <div class="text-center d-flex align-center justify-space-around">
+          <v-tooltip bottom color="primary">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                  color="primary"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                  to="/logs"
+              >
+                ログを見る
+              </v-btn>
+            </template>
+            <span>アップロードされたログを確認できます</span>
+          </v-tooltip>
+        </div>
       </v-col>
     </v-row>
   </v-container>
